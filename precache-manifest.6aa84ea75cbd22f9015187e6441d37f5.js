7242e0a8585d96fa3e1deda7a594d89a9d563537
Abraham Mamani <abraham.e.mamani@gmail.com>
2019-08-23 17:10:22 -0300

    Updates

---
+++ precache-manifest.6aa84ea75cbd22f9015187e6441d37f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b29d0386d45faa7b89d569256c6bca",
+    "url": "/weddy/index.html"
+  },
+  {
+    "revision": "d7768cb6a0e4cf0673d8",
+    "url": "/weddy/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "379d8553be433f235f51",
+    "url": "/weddy/static/css/main.a1385a06.chunk.css"
+  },
+  {
+    "revision": "d7768cb6a0e4cf0673d8",
+    "url": "/weddy/static/js/2.411d5570.chunk.js"
+  },
+  {
+    "revision": "379d8553be433f235f51",
+    "url": "/weddy/static/js/main.ae91438e.chunk.js"
+  },
+  {
+    "revision": "f041d4ae92e701dd01c1",
+    "url": "/weddy/static/js/runtime~main.9a27392f.js"
+  }
+]);
